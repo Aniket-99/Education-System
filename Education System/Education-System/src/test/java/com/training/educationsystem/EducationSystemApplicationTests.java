b7d5fda8f08034976f3715c27a19593bbc28721e
Aniket Karmakar <Aniketkarmakar89@gmail.com>
2020-12-28 14:50:10 +0530

    Spring boot starter config for education system project

---
+++ Education System/Education-System/src/test/java/com/training/educationsystem/EducationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.educationsystem.educationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
